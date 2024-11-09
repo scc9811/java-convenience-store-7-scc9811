f2b0a6cce09aedf71f4287353320046e9cc53d87
scc9811 <wjsrudtjr98@naver.com>
2024-11-10 05:24:38 +0900

    feat : 프로모션 적용 상품 안내 출력 기능 추가

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class OutputView {
     private static final String WELCOME_MESSAGE = "안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n";
     private static final String PURCHASE_MESSAGE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
+    private static final String ADD_MESSAGE = "현재 {%s}은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
 
     private OutputView() {
     }
@@ -24,5 +25,9 @@ public class OutputView {
 
     public static void printInputPurchase() {
         System.out.println(PURCHASE_MESSAGE);
+    }
+
+    public static void printInputAdd() {
+        System.out.println(ADD_MESSAGE);
     }
 }
