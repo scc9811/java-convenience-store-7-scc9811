0782541428bbcd54d0ef3f88c2e26f9f6121a7fd
scc9811 <wjsrudtjr98@naver.com>
2024-11-11 15:45:17 +0900

    refactor : 메서드 역할 분할

--- src/main/java/store/validator/ProductValidator.java
+++ src/main/java/store/validator/ProductValidator.java
@@ -11,29 +11,38 @@ public class ProductValidator {
 
     public static void validateProductExist(List<RequestItem> requestItems, List<Product> products) {
         for (RequestItem requestItem : requestItems) {
-            boolean exist = false;
-            for (Product product : products) {
-                if (requestItem.getName().equals(product.getName())) {
-                    exist = true;
-                }
-            }
+            boolean exist = isExist(products, requestItem);
             if (!exist) {
                 throw new IllegalArgumentException(ErrorMessage.NON_EXISTENT_PRODUCT.getMessage());
             }
         }
     }
 
+    private static boolean isExist(List<Product> products, RequestItem requestItem) {
+        for (Product product : products) {
+            if (requestItem.getName().equals(product.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void validateShortage(List<RequestItem> requestItems, List<Product> products) {
         for (RequestItem requestItem : requestItems) {
-            int productTotalCount = 0;
-            for (Product product : products) {
-                if (requestItem.getName().equals(product.getName())) {
-                    productTotalCount += product.getQuantity();
-                }
-            }
+            int productTotalCount = totalCount(requestItem, products);
             if (productTotalCount < requestItem.getQuantity()) {
                 throw new IllegalArgumentException(ErrorMessage.INSUFFICIENT_PRODUCT.getMessage());
             }
         }
+    }
+
+    private static int totalCount(RequestItem requestItem, List<Product> products) {
+        int productTotalCount = 0;
+        for (Product product : products) {
+            if (requestItem.getName().equals(product.getName())) {
+                productTotalCount += product.getQuantity();
+            }
+        }
+        return productTotalCount;
     }
 }
